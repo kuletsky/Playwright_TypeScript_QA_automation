1fd68085c8bcb0c324d0971c09357edabda820c0
kuletsky <kuletsky@gmail.com>
2024-09-09 19:49:59 -0400

    new scenario

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,8 +4,12 @@ const { test, expect } = require('@playwright/test');
 test.only('Verify title', async ({page}) => {
 
     await page.goto('https://automationexercise.com');
-    await expect(page).toHaveTitle('Google');     
-    await page.locator('.fa.fa-home').click()
-    // page.locator
-
+    await expect(page).toHaveTitle('Automation Exercise');     
+    await page.locator('.fa.fa-lock').click();
+    await page.locator('[data-qa="login-email"]').fill('trip27@lftjaguar.com');
+    await page.locator('[data-qa="login-password"]').fill('12314');
+    await page.locator('[data-qa="login-button"]').click();
+    let text1 = console.log(await page.locator('p[style*="color"]').textContent());
+    await expect(page.locator('p[style*="color"]')).toContainText('Your email or password is incorrect!')
+    await page.pause()
 });
