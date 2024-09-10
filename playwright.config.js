7604949538a0a64a68581a105b1913467c3227fd
kuletsky <kuletsky@gmail.com>
2024-09-09 21:41:19 -0400

    adjusting  yml

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,7 @@ module.exports = defineConfig({
   use: {
 
     browserName : 'chromium',
-    headless : false,
+    headless : true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
