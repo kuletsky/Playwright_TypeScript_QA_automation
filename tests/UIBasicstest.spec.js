a27e6dedc424edadcc10d6b63079461bc3c8c6dc
kuletsky <kuletsky@gmail.com>
2024-09-09 20:57:07 -0400

    new scenario

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,13 +3,25 @@ const { test, expect } = require('@playwright/test');
 
 test.only('Verify title', async ({page}) => {
 
+    const userName = page.locator('[data-qa="login-email"]');
+    const signIn = page.locator('[data-qa="login-button"]');
+
     await page.goto('https://automationexercise.com');
     await expect(page).toHaveTitle('Automation Exercise');     
     await page.locator('.fa.fa-lock').click();
-    await page.locator('[data-qa="login-email"]').fill('trip27@lftjaguar.com');
-    await page.locator('[data-qa="login-password"]').fill('12314');
-    await page.locator('[data-qa="login-button"]').click();
+    await userName.fill('trip27@lftjaguar.com1');
+    await page.locator('[data-qa="login-password"]').fill('1234');
+    await signIn.click();
     let text1 = console.log(await page.locator('p[style*="color"]').textContent());
-    await expect(page.locator('p[style*="color"]')).toContainText('Your email or password is incorrect!')
-    await page.pause()
+
+    await expect(page.locator('p[style*="color"]')).toContainText('Your email or password is incorrect!');
+
+    await userName.fill('');
+    await userName.fill('trip27@lftjaguar.com');
+    await signIn.click();
+
+    console.log(await page.locator('.features_items .title').textContent());
+    await expect(page.locator('.features_items .title')).toContainText('Features Items');
+
+    // await page.pause()
 });
