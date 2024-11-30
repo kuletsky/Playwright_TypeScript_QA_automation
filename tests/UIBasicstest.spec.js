27a582b7b6cc1f6fff0b77cbf5eabd353b2f15f5
kuletsky <kuletsky@gmail.com>
2024-11-30 09:28:52 -0500

    gw

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -37,7 +37,6 @@ test('Login User with correct email and password', async ({page}) => {
 
 });
 
-
 test('Verify Kafka', async({browser}) =>{
     const context = await browser.newContext();
     const page = await context.newPage();
