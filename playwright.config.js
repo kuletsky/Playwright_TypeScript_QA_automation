df9ca7353e89c1e8e00efb1fdce1688c54b2116b
kuletsky <kuletsky@gmail.com>
2024-12-13 12:12:12 -0500

    new scenario

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,8 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    browserName: 'chromium'
+    browserName: 'chromium',
+    headless: false,
   },
 
   /* Configure projects for major browsers */
--- tests/UITest.spec.js
+++ tests/UITest.spec.js
@@ -1,4 +1,5 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+const exp = require('constants');
 
 test('Browser context test',  async ({browser})=>
 {
@@ -8,9 +9,43 @@ test('Browser context test',  async ({browser})=>
 
 }); 
 
-test('Page test',  async ({page})=>
+test.only('Register User',  async ({page})=>
     {
-        await page.goto('https://google.com/'); 
-    
+        await page.goto('https://automationexercise.com/');
+        
+        // Verify the page title
+        await expect(page).toHaveTitle('Automation Exercise');
+
+        // Verify the presence of a logo
+        const logo = await page.locator('img[alt="Website for automation practice"]')
+        await expect(page.locator(logo)).toBeVisible;
+
+        // Verify URL if applicable
+        await expect(page).toHaveURL('https://automationexercise.com/')
+        
+        // Verify the New user signUp is visible 
+        await page.locator('.fa.fa-lock').click();
+        const signUp = await page.locator('.signup-form h2').textContent();
+        await expect(signUp).toBe('New User Signup!')
+
+        // Enter name and email address
+        const testData = {
+            name: 'john',
+            email: 'john@gm.com'
+        };
+
+        const signupNameInput = page.locator('[data-qa="signup-name"]');
+        const signupEmailInput = page.locator('[data-qa="signup-email"]');
+
+        // Verify that input fields contain the expected values
+        await signupNameInput.fill(testData.name);
+        await expect(signupNameInput).toHaveValue(testData.name);
+
+        await signupEmailInput.fill(testData.email);
+        await expect(signupEmailInput).toHaveValue(testData.email);
+
+        // Verify 
+        
+
     }); 
     
