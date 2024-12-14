949325c9bc2d2ea0658306bcfcaaee4f406c6ff0
kuletsky <kuletsky@gmail.com>
2024-12-13 19:53:14 -0500

    code

--- tests/UITest.spec.js
+++ tests/UITest.spec.js
@@ -1,5 +1,6 @@
 const {test, expect} = require('@playwright/test');
 const exp = require('constants');
+// const testData = require('./test-data.json')
 
 test('Browser context test',  async ({browser})=>
 {
@@ -26,7 +27,7 @@ test.only('Register User',  async ({page})=>
         // Verify the New user signUp is visible 
         await page.locator('.fa.fa-lock').click();
         const signUp = await page.locator('.signup-form h2').textContent();
-        await expect(signUp).toBe('New User Signup!')
+        await expect(signUp).toContain('New User Signup!')
 
         // Enter name and email address
         const testData = {
@@ -44,8 +45,40 @@ test.only('Register User',  async ({page})=>
         await signupEmailInput.fill(testData.email);
         await expect(signupEmailInput).toHaveValue(testData.email);
 
-        // Verify 
+        // Click 'signUp' button
+        await page.locator('[data-qa="signup-button"]').click();
         
+        // Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        const text = await page.locator('b').first().textContent();
+        await expect(text).toContain('Enter Account Information');
+        
+        // Fill detailse: Title, Name, Email, Password, Date of birth
+            
+            // Verify title radio botton works
+            const title = page.locator('#id_gender1');
+            await title.check();
+            await expect(title).toBeChecked();
+            await expect(page.locator('#id_gender2')).not.toBeChecked();
+
+            // Veryfy that Name field contains the expected name
+            const name = page.locator('#name');
+            await name.fill('');
+            await name.fill('john');
+            await expect(name).toHaveValue('john'); 
+
+            // Verify that Email field contains the expected email
+            const email = page.locator('#email');
+            await expect(email).toHaveValue('john@gm.com');
+
+            // Verify that pswd contains the expected password
+            const pswd = page.locator('#password');
+            await pswd.fill('1234');
+            await expect(pswd).toHaveValue('1234')
+
+            await page.locator('[for="newsletter"]').check();
+            await page.locator('[for="option"]').check();
+
+            // page.pause(10);
 
     }); 
     
