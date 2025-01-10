02aa4675b703a3d83f60afeb9ca06e005e3bedc7
kuletsky <kuletsky@gmail.com>
2025-01-10 11:47:20 -0500

    test

--- tests/UITest.spec.js
+++ tests/UITest.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('@playwright/test');
-const { ADDRGETNETWORKPARAMS } = require('dns');
 
 test.beforeEach(async ({ page }) => {
     await page.route('**/*', (route) => {
