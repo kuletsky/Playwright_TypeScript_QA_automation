b97830ec8503ea58ed90d6b8bd5516dd8194f237
kuletsky <kuletsky@gmail.com>
2025-01-15 10:59:10 -0500

    new test

--- tests/APiTest.spec.js
+++ tests/APiTest.spec.js
@@ -1,4 +1,5 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect, request } = require('@playwright/test');
+const exp = require('constants');
 let webContext;
 
 test.beforeAll(async ({ browser }) => {
@@ -31,4 +32,35 @@ test('Verify that user can successfuly SignIn with valid credentials', async () 
 
     // Verify that user loged in successfuly 
     await expect(page.locator('a').filter({ hasText: 'Logged in as' })).toContainText('Logged in as');
+});
+
+test.only('Fetch products list via API', async () => {
+    // Create a new API context
+    const apiContext = await request.newContext();
+
+    // Perform the GET request
+    const response = await apiContext.get('https://automationexercise.com/api/productsList');
+
+    // Check if the response status code is 200
+    expect(response.status()).toBe(200);
+
+    // Verify that the  response is in JSON format
+    const responseBody = await response.json();
+    // console.log(responseBody);
+    expect(responseBody).toBeDefined();
+
+    // Verify that the response contains fields in the expected fields
+    expect(responseBody).toHaveProperty('products');
+    expect(responseBody.products).toBeInstanceOf(Array); // Ensure it's an array of products
+
+    // Optionally, check the first product in the list
+    const firstProduct = responseBody.products[0];
+    expect(firstProduct).toHaveProperty('id');
+    expect(firstProduct).toHaveProperty('name');
+    expect(firstProduct).toHaveProperty('price');
+    expect(firstProduct).toHaveProperty('brand');
+    expect(firstProduct).toHaveProperty('category');
+
+
+
 });
